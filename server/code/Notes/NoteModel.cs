451248a232f6bb764e3f86a8788e9127afbaf622
Oliver Forral <Oliver.Forral@gmail.com>
2017-12-07 15:18:43 -0800

    Fixed some things

--- server/code/Notes/NoteModel.cs
+++ server/code/Notes/NoteModel.cs
@@ -1,14 +1,8 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-
 namespace Jirotal.Notes
 {
     public class NoteModel
     {
-        [Key]
         public long Id { get; set; }
-        [Required]
-        [MaxLength(64)]
         public string Title { get; set; }
     }
 }
